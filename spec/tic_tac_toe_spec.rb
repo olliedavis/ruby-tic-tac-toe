e7a730cc87695e5d9ddb37acdf69383ca48f5788
Ollie Davis <71295778+olliedavis@users.noreply.github.com>
2021-02-06 11:39:13 +0000

    Spec position change

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,23 +1,15 @@
 require '../lib/tic_tac_toe'
 
 describe TicTacToe do
-  subject (:tictactoe) { described_class.new }
-  describe '#valid_move?' do
-    it 'returns true if the move is valid' do
-      expect(tictactoe.valid_move?(4)).to be true
-    end
-    it 'returns false if the move is false' do
-      expect(tictactoe.valid_move?(15)).to be false
-    end
-  end
+  subject(:tictactoe) { described_class.new }
 
-  describe '#move' do
+  describe 'move' do
     it 'updates the board with the new move' do
       expect{ tictactoe.move(0) }.to change{ tictactoe.board[0] }.from(' ').to('X')
     end
   end
 
-  describe '#position_taken?' do
+  describe 'position_taken?' do
     before do
       tictactoe.board[0] = 'X'
     end
@@ -30,4 +22,14 @@ describe TicTacToe do
       expect(tictactoe.position_taken?(1)). to be false
     end
   end
+
+  describe 'valid_move?' do
+    it 'returns true if the move is valid' do
+      expect(tictactoe.valid_move?(4)).to be true
+    end
+    it 'returns false if the move is false' do
+      expect(tictactoe.valid_move?(15)).to be false
+    end
+  end
+  
 end
